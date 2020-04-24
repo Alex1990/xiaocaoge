558ccf91f14b47bbbd36ad77040e6c8c60871c9d
Alex Chao <alexchao1990@gmail.com>
2020-04-24 22:40:03 +0800

    修复 manifest icon 配置

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,18 +81,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "minimal-ui",
-        icons: [
-          {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
+        icon: "static/favicon/apple-touch-icon-152x152.png"
       }
     },
     "gatsby-plugin-offline",
