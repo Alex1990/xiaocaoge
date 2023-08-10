9bd88ab9e5626c2d6e164ae28ba86ecfb8dba0b6
Alex Chao <alexchao1990@gmail.com>
2023-08-11 01:18:32 +0800

    升级为 GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,9 +55,11 @@ module.exports = {
       }
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: config.googleAnalyticsID
+        trackingIds: [
+          config.googleAnalyticsID
+        ],
       }
     },
     {
